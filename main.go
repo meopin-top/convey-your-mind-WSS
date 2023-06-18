59f212185ff0972f43f76a335c48307b90e841e5
손병욱 <bwson98@gmail.com>
2023-06-18 19:23:11 +0000

    feat: 웹소캣 메인 로직 구현

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/gofiber/contrib/websocket"
@@ -17,8 +18,8 @@ type Payload struct {
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.SendString("pong")
 	})
 
 	app.Use("/ws", func(c *fiber.Ctx) error {
@@ -29,6 +30,87 @@ func main() {
 		return fiber.ErrUpgradeRequired
 	})
 
+	app.Get("/ws/:channel_id", websocket.New(func(c *websocket.Conn) {
+		channelID := c.Params("channel_id")
+		subscribe(channelID)
+
+		defer func() {
+			unsubscribe(channelID)
+		}()
+
+		// Get local value
+		allowed := c.Locals("allowed").(bool)
+
+		log.Println(channelID, allowed)
+
+		go func() {
+			// when broadcast is called get message from channel and send to client
+			for {
+				msg := <-channel
+
+				jsonMsg, err := json.Marshal(msg)
+				if err != nil {
+					log.Println("marshal:", err)
+					break
+				}
+
+				// send message to client
+				err = c.WriteMessage(websocket.TextMessage, jsonMsg)
+				if err != nil {
+					log.Println("write:", err)
+					break
+				}
+
+				log.Printf("sent: %s\n", jsonMsg)
+			}
+		}()
+
+		for {
+			mt, msg, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				break
+			}
+
+			var payload Payload
+			err = json.Unmarshal(msg, &payload)
+			if err != nil {
+				log.Println("unmarshal:", err)
+				break
+			}
+
+			// update channel with new message
+			updateChannel(channelID, payload)
+
+			// Send message to all subscribers
+			broadcast(channelID, payload)
+
+			log.Printf("recv: %s\n", msg)
+			err = c.WriteMessage(mt, msg)
+			if err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+
+	}))
+
 	log.Fatal(app.Listen(":3000"))
-	// Access ws://localhost:3000/ws/123?v=1.0
+	// Access ws://localhost:3000/ws/{channel_id}
+}
+
+func unsubscribe(channelID string) {
+	panic("unimplemented")
+}
+
+func broadcast(channelID string, payload Payload) {
+	panic("unimplemented")
+}
+
+func updateChannel(channelID string, payload Payload) {
+	panic("unimplemented")
+}
+
+func subscribe(channelID string) {
+	panic("unimplemented")
 }
