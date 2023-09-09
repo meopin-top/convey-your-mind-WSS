ca29b4fa7910e42018e17dd1755d8e103a6e0a81
j1mmyson <bwson98@gmail.com>
2023-09-09 20:59:55 +0900

    feat: payload 좌표 속성 추가

--- domain/payload.go
+++ domain/payload.go
@@ -2,8 +2,29 @@ package domain
 
 // Payload is the data structure for the websocket
 type Payload struct {
-	PaperID     string `json:"paper_id"`
+	ProjectID string  `json:"project_id"`
+	UserID    string  `json:"user_id"`
+	Content   Content `json:"content"`
+}
+
+// Content is the data structure for the websocket
+type Content struct {
+	// status
+	// json camel case
 	UserID      string `json:"user_id"`
-	Message     string `json:"message"`
-	MessageType string `json:"message_type"`
+	ContentID   string `json:"content_id"`
+	ContentType string `json:"content_type"`
+	X           int    `json:"x"`
+	Y           int    `json:"y"`
+	Width       int    `json:"width"`
+	Height      int    `json:"height"`
+	Text        string `json:"text"`
+	ImageURL    string `json:"image_url"` // could be empty
+}
+
+// Project is the data structure for the project
+type Project struct {
+	Status    string    `json:"status"` // will be enum later
+	ProjectID string    `json:"project_id"`
+	Contents  []Content `json:"contents"`
 }
